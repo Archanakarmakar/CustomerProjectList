d946705c6c76c5c10dc40656cd5a7205d4c3259a
Sumit Karmakar <archanakmr83@gmail.com>
2019-12-06 09:46:41 -0600

    did add testing

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -24,7 +24,79 @@ namespace UnitTestProject1
             //assert
             Assert.AreEqual(expectedResult, newlist[3]);
         }
+        [TestMethod]
+        public void Add_String()
+        {
+            NewCustomList<string> newlist = new NewCustomList<string>();
+            string expectedResult = "Test";
+
+            newlist.Add("words");
+            newlist.Add("Test");
+
+            //assert
+            Assert.AreEqual(expectedResult, newlist[1]);
+        }
+        [TestMethod]
+        public void Add_Double()
+        {
+            NewCustomList<double> newlist = new NewCustomList<double>();
+            double expectedResult = 19.55;
+            //act
+            newlist.Add(.32);
+            newlist.Add(19.55);
+
+
+            //assert
+            Assert.AreNotEqual(expectedResult, newlist[0]);
+        }
+
+
+
+        [TestMethod]
+        public void Add_SetStringAtIndex0()
+        {
+            NewCustomList<string> newlist = new NewCustomList<string>();
+            newlist.Add("hello");
+            string bar = "foo";
+            //Act
+            newlist[0] = bar;
+
+            //Assert
+            Assert.AreEqual("foo", bar);
+        }
+        [TestMethod]
+        public void Add_IndexCpacity()
+        {
+            NewCustomList<int> newlist = new NewCustomList<int>();
+            newlist.Add(5);
+            newlist.Add(3);
+            newlist.Add(5);
+            newlist.Add(22);
+            newlist.Add(50);
+            newlist.Add(2);
+            newlist.Add(100);
+            int expected = 10;
+            int actual = newlist.Capacity;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_IndexCount()
+        {
+            NewCustomList<int> newlist = new NewCustomList<int>();
+            newlist.Add(5);
+            newlist.Add(3);
+            newlist.Add(22);
+            newlist.Add(50);
+            newlist.Add(2);
+            newlist.Add(100);
+            int expected = 6;
+            int actual = newlist.Count;
+
+            Assert.AreEqual(expected, actual);
+        }
 
     }
     }
+
 
